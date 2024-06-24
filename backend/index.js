6d10751287dc871e93490f701b66812d2d690351
Mark <markporcarojr@gmail.com>
2024-06-24 17:33:57 -0400

    Update cors Policy

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ connectDB();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://hive-tool.netlify.app",
         methods: "GET, POST, PUT, DELETE, PATCH",
         credentials: true,
     })
