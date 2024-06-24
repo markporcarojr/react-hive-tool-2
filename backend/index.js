82bf07958421c80f8a40934b12b7164173f6b317
Mark <markporcarojr@gmail.com>
2024-06-24 18:55:09 -0400

    cors bypass for testing index.js

--- backend/index.js
+++ backend/index.js
@@ -35,16 +35,22 @@ connectDB();
 
 // ****************************************  MiddleWare  **************************************
 
-app.use(express.json());
+// app.use(express.json());
+// app.use(
+//     cors({
+//         origin: ["https://hive-tool.netlify.app", "https://api.openweathermap.org"],
+//         methods: "GET, POST, PUT, DELETE, PATCH",
+//         credentials: true,
+//     })
+// );
+
 app.use(
     cors({
-        origin: ["https://hive-tool.netlify.app", "https://api.openweathermap.org"],
+        origin: "*",
         methods: "GET, POST, PUT, DELETE, PATCH",
         credentials: true,
     })
 );
-
-
 
 app.use(session({
     store,
