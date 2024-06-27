836857e309e33ae1d843f724bf84f96b247c9055
Mark <markporcarojr@gmail.com>
2024-06-27 12:31:15 -0400

    Added a honeybee as my spinner!

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,16 +1,38 @@
-import Spinner from "react-bootstrap/Spinner";
+// import Spinner from "react-bootstrap/Spinner";
+
+// const LoadSpinner = () => {
+//   return (
+//     <div className="d-flex justify-content-center align-items-center vh-100">
+//       <Spinner
+//         animation="border"
+//         variant="warning"
+//         role="status"
+//         style={{ width: "10vw", height: "10vw" }}
+//       >
+//         <span className="visually-hidden">Loading...</span>
+//       </Spinner>
+//     </div>
+//   );
+// };
+
+// export default LoadSpinner;
+
+import Lottie from "react-lottie";
+import animationData from "../assets/animations/honeyBee.json";
 
 const LoadSpinner = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <Spinner
-        animation="border"
-        variant="warning"
-        role="status"
-        style={{ width: "10vw", height: "10vw" }}
-      >
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
     </div>
   );
 };
