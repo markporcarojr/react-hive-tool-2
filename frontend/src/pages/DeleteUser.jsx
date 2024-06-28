e9a881ace4f588ebb578809f46c6c137f7e5dfbc
Mark <markporcarojr@gmail.com>
2024-06-28 17:47:18 -0400

    Update loadspinner on DeleteUser.jsx

--- frontend/src/pages/DeleteUser.jsx
+++ frontend/src/pages/DeleteUser.jsx
@@ -48,27 +48,34 @@ const DeleteUser = () => {
 
   return (
     <>
-      {loading && <LoadSpinner />}
-      <CustomNavbar />
-      <Card
-        className="rounded-5 border-3 d-flex align-items-center p-5 m-5"
-        style={{ borderColor: "#ffcb05" }}
-      >
-        <h1 className="fs-1 fw-bold my-4 text-white mb-5">Delete Account</h1>
-        {loading ? <LoadSpinner /> : ""}
-        <div className="d-flex align-items-center mx-auto text-michgold">
-          <h3>Are You Sure You Want To Delete Your Account?</h3>
-        </div>
-        <div>
-          <Button className="btn-danger m-3" onClick={handleDeleteUser}>
-            YES
-          </Button>
-          <BackButton text={"NO"} />
-        </div>
-        <p className="text-danger">{message}</p>
-      </Card>
-      ;
-      <Footer />
+      {loading ? (
+        <LoadSpinner />
+      ) : (
+        <>
+          <CustomNavbar />
+          <Card
+            className="rounded-5 border-3 d-flex align-items-center p-5 m-5"
+            style={{ borderColor: "#ffcb05" }}
+          >
+            <h1 className="fs-1 fw-bold my-4 text-white mb-5">
+              Delete Account
+            </h1>
+            {/* {loading ? <LoadSpinner /> : ""} */}
+            <div className="d-flex align-items-center mx-auto text-michgold">
+              <h3>Are You Sure You Want To Delete Your Account?</h3>
+            </div>
+            <div>
+              <Button className="btn-danger m-3" onClick={handleDeleteUser}>
+                YES
+              </Button>
+              <BackButton text={"NO"} />
+            </div>
+            <p className="text-danger">{message}</p>
+          </Card>
+          ;
+          <Footer />
+        </>
+      )}
     </>
   );
 };
